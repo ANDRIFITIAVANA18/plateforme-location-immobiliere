752a4b467ebadbc0d8e4b9fb0364b8af3f41b1b2
Isaia Andrifitiavana <razafindrabeandrifitiavana@gmail.com>
2025-11-21 14:53:37 +0300

    clean: file

--- test-error-reelle.ts
+++
@@ -1,4 +0,0 @@
-﻿// 🧪 TEST ERREUR RÉELLE
-const erreurReelle: string = 123; // Cette ligne devrait être détectée
-const autreErreur: number = 'test'; // Cette aussi
-console.log('Test erreurs réelles');
